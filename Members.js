b44b3b97e0b951df96d0402104e27bcfdfe8b63a
Sagir Garba <sagir40me@gmail.com>
2021-02-13 14:50:34 +0100

    Initial Commit

---
+++ Members.js
@@ -0,0 +1,25 @@
+const members = [
+    {
+        id: 1,
+        name: "Bello Tupac",
+        email: "hayatee@gmail.com",
+        status: "active"
+    },
+
+    {
+        id: 2,
+        name: "Salimat Yusuf",
+        email: "Farhatee@gmail.com",
+        status: "Inactive"
+    },
+
+    {
+        id: 3,
+        name: "Ahmad Muhd",
+        email: "Master@gmail.com",
+        status: "active"
+    }
+];
+
+
+module.exports = members;
